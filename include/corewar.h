2e427cd7505e2c1cebc208ec401a04400e8fa8c9
Gaël Laguna <gael.laguna@epitech.eu>
2025-04-19 22:07:42 +0200

    💩 Basic args check

--- include/corewar.h
+++ include/corewar.h
@@ -8,6 +8,29 @@
 #ifndef COREWAR_H_
     #define COREWAR_H_
 
+    #include <stdbool.h>
+    #include "my_string.h"
+    #include "my_stype.h"
+    #include "my_stdlib.h"
+
+    // Args handling
+    #define USAGE_ARG "-h"
+    #define DUMP_ARG "-dump"
+
 void display_usage(void);
+
+typedef struct program_data_s {
+    char *prog_name;
+    int prog_number;
+    int prog_adress;
+} program_data_t;
+
+typedef struct corewar_data_s {
+    program_data_t *programs;
+    long nbr_cycle;
+    bool usage;
+    bool graphical_env;
+    char padding[2];
+} corewar_data_t;
 
 #endif /* COREWAR_H_ */
--- src/main.c
+++ src/main.c
@@ -6,9 +6,50 @@
 */
 
 #include "corewar.h"
+#include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char **argv, char **env)
+static corewar_data_t *initialize_data(
+    int argc,
+    char **argv,
+    char **env)
 {
-    display_usage();
+    corewar_data_t *data = my_calloc(1, sizeof(corewar_data_t));
+    char *tmp;
+
+    for (int i = 1; i < argc; i++) {
+        if (!my_strcmp(argv[i], USAGE_ARG)) {
+            data->usage = true;
+            return data;
+        }
+        if (!my_strcmp(argv[i], DUMP_ARG) && i + 1 < argc) {
+            data->nbr_cycle = my_strtol(argv[i + 1], &tmp, 16);
+            i++;
+            continue;
+        }
+        write(STDERR_FILENO, "Incorrect argument\n", 19);
+        display_usage();
+        return NULL;
+    }
+    return data;
+}
+
+int main(
+    int argc,
+    char **argv,
+    char **env)
+{
+    corewar_data_t *data = initialize_data(argc, argv, env);
+
+    if (!data || !data->nbr_cycle) {
+        free_garbage();
+        return 84;
+    }
+    if (data->usage) {
+        display_usage();
+        free_garbage();
+        return 0;
+    }
+    free_garbage();
     return 0;
 }
