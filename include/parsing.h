361770562e8e42e2eb76b98a31ceba4b355cb3da
arkcadia <vbortesgueva@gmail.com>
2025-04-29 22:25:20 +0200

    :sparkles: added (index management) new prototype for index check + define for indexes for better lisibility

--- include/parsing.h
+++ include/parsing.h
@@ -15,14 +15,18 @@
     #define PROG_START 2192
     #define P_DIRECT 10
     #define P_REG 1
+    #define P_INDEX 2
     #define P_INDIRECT 11
     #define NOTHING 0
+    #define INDEX_NBR 1
+    #define MAX_ARGS 3
     #define IGNORE __attribute__((unused))
 typedef struct {
     int prog_size;
     struct instruction_s **instruction;
 } parsing_data_t;
 
+int index_check(instruction_t *instruction);
 int special_inst(instruction_t *instruction, char *bin);
 void debug_info(parsing_data_t *parse_data);
 void int_to_bin(int n, char output[9]);
