faf5cdff1b266baf61a876c9e63b2ebc4c52b5f8
arkcadia <vbortesgueva@gmail.com>
2025-04-29 22:27:50 +0200

    :sparkles: added (index_check) function to check if the instruction has indexes. Later be used for labels

--- src/parsing/parse_special_params.c
+++ src/parsing/parse_special_params.c
@@ -4,19 +4,30 @@
 ** File description:
 ** header
 */
-#include "../../include/op.h"
 #include "../../include/corewar.h"
 #include "../../include/parsing.h"
 #include <stddef.h>
 #include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/types.h>
+
+int index_check(
+    instruction_t *instruction)
+{
+    int list_index[] = {11};
+
+    for (int i = 0; i != INDEX_NBR; i++) {
+        if (instruction->op_code == list_index[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int special_inst(
     instruction_t *instruction,
     char *bin)
 {
+    index_check(instruction);
     if (instruction->op_code == 12) {
         instruction->params[0].index = (uint16_t)(bin[0] + bin[1]);
         instruction->param_type[0] = PARAM_INDEX;
